154ad4b77128a22c1cbd896710cdda7030a6bbcd
Aurel Marishta <aurel.marishta@gmail.com>
2020-10-24 19:39:57 +0200

    initial commit

---
+++ src/main/java/al/marishta/travelly/TravellyApplication.java
@@ -0,0 +1,13 @@
+package al.marishta.travelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravellyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/al/marishta/travelly/TravellyApplicationTests.java
@@ -0,0 +1,13 @@
+package al.marishta.travelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
